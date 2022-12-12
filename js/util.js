e43b761ab6c7ffe19b3db9f1fdac78a5cb432c79
yontare <shulga.d@digdes.com>
2022-12-12 18:02:14 +0300

    add: module12-task1

--- js/util.js
+++ js/util.js
@@ -36,6 +36,10 @@ function getRandomFromArr(arr) {
   return arr[getRandomNumber(0, arr.length - 1)];
 }
 
+function getRandomElementsFromArr(array, n) {
+  return array.sort(() => Math.random() - Math.random()).slice(0, n);
+}
+
 function getId() {
   return uuidv4();
 }
@@ -45,4 +49,25 @@ function getPictureId() {
   return pictureCounter;
 }
 
-export {getRandomNumber, validateComment, validateHashTags, getRandomFromArr, getId, getPictureId};
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+export {getRandomElementsFromArr, getRandomNumber, validateComment, validateHashTags, getRandomFromArr, getId, getPictureId, debounce};
